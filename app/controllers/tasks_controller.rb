0a5d2f4755a88826b6745846e0c2894f0ecdfee2
ykatsuro <yu_katsuro@hotmail.com>
2017-06-08 10:24:48 +0000

    x

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,31 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
   
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+  
   def new
     @task = task.new(task_params)
     
@@ -21,6 +46,14 @@ class TasksController < ApplicationController
   
   def edit
     @task = Task.find(params[:id])
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to task_url
   end
   
   # Strong Parameter
