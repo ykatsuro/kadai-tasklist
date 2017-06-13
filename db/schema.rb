36c977a3e7a4ad90cdd6f6858cc85d74263ef767
ykatsuro <yu_katsuro@hotmail.com>
2017-06-13 09:31:53 +0000

    DRY

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613025637) do
+ActiveRecord::Schema.define(version: 20170613091300) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "title"
+    t.string   "status"
   end
 
 end
