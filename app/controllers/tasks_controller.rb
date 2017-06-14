062e88232db19f15cc4224cba35d2352c9485bb2
ykatsuro <yu_katsuro@hotmail.com>
2017-06-14 15:33:11 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all.page(params[:page])
+    @tasks = Task.all
   end
   
   def new
