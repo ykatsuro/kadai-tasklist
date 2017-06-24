5717636cc36b18789a281dd5bb6cb40a0bf84c1d
ykatsuro <yu_katsuro@hotmail.com>
2017-06-24 14:48:39 +0000

    DRY

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'sessions/new'
 
-  get 'users/show'
+  get 'sessions/create'
 
-  get 'users/new'
+  get 'sessions/destroy'
 
-  get 'users/create'
-
-  root to: 'tasks#index'
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
