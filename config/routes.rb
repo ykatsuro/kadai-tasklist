788a5055358238348b91dfb2838ac497caed1699
ykatsuro <yu_katsuro@hotmail.com>
2017-06-24 13:01:37 +0000

    DRY

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
 
   root to: 'tasks#index'
   
-  resources :tasks
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
