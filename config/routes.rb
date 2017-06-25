4fab9d6785918f575bbb1920b4fde6cd9467c32d
ykatsuro <yu_katsuro@hotmail.com>
2017-06-25 15:10:39 +0000

    DRY

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users
+  resources :users, only: [:index, :show, :new, :create]
   
   resources :tasks
 end
