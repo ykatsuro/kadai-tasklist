3bbcafb7d7235defebbf38a181bc3d10c3b29f06
ykatsuro <yu_katsuro@hotmail.com>
2017-06-25 08:01:25 +0000

    DRY

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @user = current_user
-      @tasklist = current_user.tasklists.build # form_for用
+      @tasklist = current_user.tasklists.build  # form_for 用
       @tasklists = current_user.tasklists.order('created_at DESC').page(params[:page])
     end
   end
