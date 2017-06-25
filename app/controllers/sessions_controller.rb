af43b8f23b4fb80ee4640a1af8e1b3cb6ccff4e9
ykatsuro <yu_katsuro@hotmail.com>
2017-06-25 07:58:05 +0000

    DRY

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,7 +24,7 @@ class SessionsController < ApplicationController
   
   def login(email, password)
     @user = User.find_by(email: email)
-    if @user && @user.find.authenticate(password)
+    if @user && @user.authenticate(password)
       # ログイン成功
       session[:user_id] = @user.id
       return true
