6002f47325506f73518b7784dd0ca95acbee4f96
ykatsuro <yu_katsuro@hotmail.com>
2017-06-26 12:52:32 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action ::require_user_logged_in
+  
   def index
     @tasks = Task.all
   end
@@ -13,12 +14,12 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
