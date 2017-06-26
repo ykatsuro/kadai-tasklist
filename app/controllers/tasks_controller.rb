a547efca856987b5073cb6b48a8a8a14a50211b7
ykatsuro <yu_katsuro@hotmail.com>
2017-06-26 13:05:47 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   
-  def index
-    @tasks = Task.all
-  end
-  
-  def new
-    @task = Task.new
-  end
-
-  def show
-    @task = Task.find(params[:id])
-  end
-  
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'Task を投稿しました'
+      redirect_to root_url
     else
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
