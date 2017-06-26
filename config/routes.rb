37abc0b0033e83ae6e0736680c97072e892664f0
ykatsuro <yu_katsuro@hotmail.com>
2017-06-26 13:15:35 +0000

    DRY

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
 
   get 'login', to: 'sessions#new'
-  post 'login', to: 'tasks#index'
+  post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
