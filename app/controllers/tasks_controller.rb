48c0f591f79f79333478e71ac4e558bf36a06cf3
ykatsuro <yu_katsuro@hotmail.com>
2017-06-28 13:21:54 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,6 +32,14 @@ class TasksController < ApplicationController
 
   def edit
     @task = Task.find(params[:id])
+    
+    if @task.edit(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
   end
   
   def destroy
